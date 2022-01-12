5b42c24be3a3b2544a41e93b389db188afcad580
HamzaEg <h.egnaidi@gmail.com>
2022-01-12 12:50:36 +0100

    to egnaidi.xyz

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import HomeIcon from '@mui/icons-material/Home';
+import { Home, Engineering } from '@mui/icons-material';
 import { Link } from 'react-router-dom'
 
 export default function DenseAppBar() {
@@ -12,9 +12,14 @@ export default function DenseAppBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar variant="dense">
+        <Link to='www.egnaidi.xyz'>
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <Engineering sx={{color:"white"}}/>
+          </IconButton>
+        </Link>
         <Link to={'/'}>
           <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <HomeIcon sx={{color:"white"}}/>
+            <Home sx={{color:"white"}}/>
           </IconButton>
         </Link>
           <Typography variant="h6" color="inherit" component="div">
